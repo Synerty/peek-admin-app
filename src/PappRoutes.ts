3e642e3cb96dad57ace05f4661a25d75c85737e6
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-10 19:10:00 +1000

    Moved most files under the root python package for each service. Python packages are deployed under their root package name so this makes it easier/possible to package.

--- src/PappRoutes.ts
+++
@@ -1,8 +0,0 @@
-// This file is auto generated, the git version is blank and .gitignored
-export const pappRoutes = [
-
-                {
-                    path: 'papp_noop',
-                    loadChildren: "papp_noop/papp-noop.module#default"
-                }
-                ];
