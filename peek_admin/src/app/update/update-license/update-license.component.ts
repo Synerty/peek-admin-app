0322308282624d2b10a6283615292067891ea7f0
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-30 23:38:37 +1000

    Updated for compatibility with new PayloadEnvelope Vortex update
    ORMOB-614

--- peek_admin/src/app/update/update-license/update-license.component.ts
+++ peek_admin/src/app/update/update-license/update-license.component.ts
@@ -1,63 +1,66 @@
 import {Component, OnInit} from "@angular/core";
 import {
-    VortexService,
-    ComponentLifecycleEventEmitter,
-    TupleLoader,
-    Tuple,
-    Payload
+  ComponentLifecycleEventEmitter,
+  Payload,
+  PayloadEnvelope,
+  Tuple,
+  TupleLoader,
+  VortexService
 } from "@synerty/vortexjs";
 import {Ng2BalloonMsgService} from "@synerty/ng2-balloon-msg";
 
 @Component({
-    selector: 'app-update-license',
-    templateUrl: './update-license.component.html',
-    styleUrls: ['./update-license.component.css']
+  selector: 'app-update-license',
+  templateUrl: './update-license.component.html',
+  styleUrls: ['./update-license.component.css']
 })
 export class UpdateLicenseComponent extends ComponentLifecycleEventEmitter implements OnInit {
-    private readonly filt = {
-        plugin: "peek_server",
-        key: "admin.capabilities.data"
-    };
+  private readonly filt = {
+    plugin: "peek_server",
+    key: "admin.capabilities.data"
+  };
 
-    data: {} = {};
-    private loader: TupleLoader;
+  data: {} = {};
+  private loader: TupleLoader;
 
-    // @Output("licenced")
-    // licencedOutput : EventEmitter<boolean> = new EventEmitter<boolean>();
-    licenced: boolean = false;
+  // @Output("licenced")
+  // licencedOutput : EventEmitter<boolean> = new EventEmitter<boolean>();
+  licenced: boolean = false;
 
-    constructor(private vortexService: VortexService,
-                private balloonMsg: Ng2BalloonMsgService) {
-        super();
+  constructor(private vortexService: VortexService,
+              private balloonMsg: Ng2BalloonMsgService) {
+    super();
 
-        vortexService.createEndpointObservable(this, this.filt)
-            .subscribe(payload => this.processPayload(payload));
+    vortexService.createEndpointObservable(this, this.filt)
+      .subscribe((payloadEnvelope: PayloadEnvelope) =>
+        this.processPayload(payloadEnvelope)
+      );
 
-        // Trigger the server to send the latest data
-        vortexService.sendFilt(this.filt);
-    }
+    // Trigger the server to send the latest data
+    vortexService.sendFilt(this.filt);
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    private processPayload(payload: Payload): void {
-        if (payload.result) {
-            if (payload.result["success"])
-                this.balloonMsg.showSuccess(payload.result["message"]);
-            else
-                this.balloonMsg.showError(payload.result["message"]);
-        }
+  private processPayload(payloadEnvelope: PayloadEnvelope): void {
+    // if (payloadEnvelope.result) {
+    //   if (payloadEnvelope.result["success"])
+    //     this.balloonMsg.showSuccess(payloadEnvelope.result["message"]);
+    //   else
+    //     this.balloonMsg.showError(payloadEnvelope.result["message"]);
+    // }
+    //
+    // if (payload.tuples.length) {
+    //   this.data = payload.tuples[0].data;
+    //   this.licenced = !(this.data["supportExceeded"]
+    //     && !this.data["demoExceeded"]);
+    // }
+  }
 
-        if (payload.tuples.length) {
-            this.data = payload.tuples[0].data;
-            this.licenced = !(this.data["supportExceeded"]
-                                && !this.data["demoExceeded"]);
-        }
-    }
-
-    updateLicense() {
-        let dataWrapTuple = new Tuple('c.s.r.datawraptuple');
-        dataWrapTuple["data"] = this.data["newkey"];
-        this.vortexService.sendTuple(this.filt, [dataWrapTuple]);
-    }
+  updateLicense() {
+    let dataWrapTuple = new Tuple('c.s.r.datawraptuple');
+    dataWrapTuple["data"] = this.data["newkey"];
+    this.vortexService.sendTuple(this.filt, [dataWrapTuple]);
+  }
 }
