f0342aa6f2b420fbc47e8dca4fc04484217b7bb3
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:28 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_admin/__init__.py
+++ peek_admin/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '###PEEKVER###'
+__version__ = '1.1.1'
---
+++ publish.settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_admin"
+PYPI_PUBLISH="1"
+
+VER_FILES=""
+VER_FILES="${VER_FILES} peek_admin/src/app/navbar/navbar.component.html"
