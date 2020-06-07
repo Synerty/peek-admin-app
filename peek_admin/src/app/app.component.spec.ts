84d67ce19daab6ece43257ec9a646f43b3e397c6
Romaan Ahmed <romaan.ahmed@synerty.com>
2020-06-07 10:01:07 +1000

    orpeek-198 Updating package.json for ng-zorro and cleaned the angular spec

--- peek_admin/src/app/app.component.spec.ts
+++ peek_admin/src/app/app.component.spec.ts
@@ -4,19 +4,19 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-  });
-
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [
+  //       AppComponent
+  //     ],
+  //   });
+  // });
+  //
+  // it('should create the app', async(() => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   let app = fixture.debugElement.componentInstance;
+  //   expect(app).toBeTruthy();
+  // }));
 
   // it(`should have as title 'app works!'`, async(() => {
   //   let fixture = TestBed.createComponent(AppComponent);
