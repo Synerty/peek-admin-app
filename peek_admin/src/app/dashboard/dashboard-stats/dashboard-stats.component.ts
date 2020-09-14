c87a118a3020509af873c07d13c454ff883cbf9c
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-09-15 09:22:55 +1000

    PEEK-671 Refactor ComponentLifeCycleEventEmitter class

--- peek_admin/src/app/dashboard/dashboard-stats/dashboard-stats.component.ts
+++ peek_admin/src/app/dashboard/dashboard-stats/dashboard-stats.component.ts
@@ -1,50 +1,49 @@
-import {Component, OnInit, EventEmitter} from "@angular/core";
-import {
-  VortexService,
-  VortexStatusService,
-  ComponentLifecycleEventEmitter,
-  TupleLoader
-} from "@synerty/vortexjs";
+import { Component } from "@angular/core"
+import { VortexService, VortexStatusService } from "@synerty/vortexjs"
+import { NgLifeCycleEvents } from "@synerty/peek-plugin-base-js"
 
 interface Stat {
-  desc: string;
-  value: string;
+    desc: string;
+    value: string;
 }
 
 @Component({
-  selector: 'app-dashboard-stats',
-  templateUrl: './dashboard-stats.component.html',
-  styleUrls: ['./dashboard-stats.component.css']
+    selector: "app-dashboard-stats",
+    templateUrl: "./dashboard-stats.component.html",
+    styleUrls: ["./dashboard-stats.component.css"]
 })
-export class DashboardStatsComponent extends ComponentLifecycleEventEmitter {
-  private readonly statsFilt = {
-    plugin: 'peek_server',
-    key: "peakadm.dashboard.list.data"
-  };
-
-  // stats: Stat[] = [];
-  // loader: TupleLoader;
-
-  constructor(vortexService: VortexService, vortexStatus: VortexStatusService) {
-    super();
-
-    // this.loader = vortexService.createTupleLoader(this, this.statsFilt);
-    //
-    //
-    // vortexStatus
-    //   .isOnline
-    //   .filter(online => online)
-    //   .first()
-    //   .subscribe(() => {
-    //     this.loader.observable.subscribe(
-    //       tuples => {
-    //         this.stats = <Stat[]>tuples;
-    //         this.stats.sort((a, b) => {
-    //           return (<Stat>a).desc.localeCompare((<Stat>b).desc);
-    //         });
-    //       });
-    //   });
-
-  }
-
+export class DashboardStatsComponent extends NgLifeCycleEvents {
+    private readonly statsFilt = {
+        plugin: "peek_server",
+        key: "peakadm.dashboard.list.data"
+    }
+    
+    // stats: Stat[] = [];
+    // loader: TupleLoader;
+    
+    constructor(
+        vortexService: VortexService,
+        vortexStatus: VortexStatusService
+    ) {
+        super()
+        
+        // this.loader = vortexService.createTupleLoader(this, this.statsFilt);
+        //
+        //
+        // vortexStatus
+        //   .isOnline
+        //   .filter(online => online)
+        //   .first()
+        //   .subscribe(() => {
+        //     this.loader.observable.subscribe(
+        //       tuples => {
+        //         this.stats = <Stat[]>tuples;
+        //         this.stats.sort((a, b) => {
+        //           return (<Stat>a).desc.localeCompare((<Stat>b).desc);
+        //         });
+        //       });
+        //   });
+        
+    }
+    
 }
--- peek_admin/src/app/navbar/navbar.component.ts
+++ peek_admin/src/app/navbar/navbar.component.ts
@@ -1,91 +1,75 @@
-import {Component, OnInit} from "@angular/core";
-import {
-    ComponentLifecycleEventEmitter,
-    Payload,
-    Tuple,
-    VortexService,
-    VortexStatusService
-} from "@synerty/vortexjs";
-import {
-    dashboardRoute
-} from "../app-routing.module";
-import {homeLinks} from "../../plugin-home-links";
-
+import { Component, OnInit } from "@angular/core"
+import { Payload, Tuple, VortexService, VortexStatusService } from "@synerty/vortexjs"
+import { dashboardRoute } from "../app-routing.module"
+import { homeLinks } from "../../plugin-home-links"
+import { NgLifeCycleEvents } from "@synerty/peek-plugin-base-js"
 
 class UserTuple extends Tuple {
+    supportExceeded: boolean = false
+    demoExceeded: boolean = true
+    countsExceeded: boolean = true
+    username: string = "None"
+    
     constructor() {
-        super('peek_server.PeekAdmNavbarUserTuple');
+        super("peek_server.PeekAdmNavbarUserTuple")
     }
-
-    supportExceeded: boolean = false;
-    demoExceeded: boolean = true;
-    countsExceeded: boolean = true;
-    username: string = "None";
 }
 
-
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css']
+    selector: "app-navbar",
+    templateUrl: "./navbar.component.html",
+    styleUrls: ["./navbar.component.css"]
 })
-export class NavbarComponent extends ComponentLifecycleEventEmitter implements OnInit {
-
-    // -------------- Load User Details
-    private readonly userDataFilt = {
-        plugin: 'peek_server',
-        key: "nav.adm.user.data"
-    };
-
-    dashboardPath: string = dashboardRoute.path;
-
-    user: UserTuple = new UserTuple();
-
+export class NavbarComponent extends NgLifeCycleEvents implements OnInit {
+    dashboardPath: string = dashboardRoute.path
+    user: UserTuple = new UserTuple()
+    // Make it public because AppRouterModule uses it as well
+    platformMenuData = []
+    
     // ----------- Load Core Plugin Menu Items
     // Make it public because AppRouterModule uses it as well
-    platformMenuData = [];
-
+    pluginsMenuData = []
+    
     // ----------- Load Optional Plugin Menu Items
-    // Make it public because AppRouterModule uses it as well
-    pluginsMenuData = [];
-
-    vortexIsOnline: boolean = false;
-
-    private tupleLoader = null;
-
-    constructor(vortexStatusService: VortexStatusService,
-                private vortexService: VortexService) {
-        super();
-
+    vortexIsOnline: boolean = false
+    // -------------- Load User Details
+    private readonly userDataFilt = {
+        plugin: "peek_server",
+        key: "nav.adm.user.data"
+    }
+    private tupleLoader = null
+    
+    constructor(
+        vortexStatusService: VortexStatusService,
+        private vortexService: VortexService
+    ) {
+        super()
+        
         for (let homeLink of homeLinks) {
             if (homeLink.name.startsWith("peek_core")) {
-                this.platformMenuData.push(homeLink);
-            } else {
-                this.pluginsMenuData.push(homeLink);
+                this.platformMenuData.push(homeLink)
+            }
+            else {
+                this.pluginsMenuData.push(homeLink)
             }
         }
-
+        
         vortexStatusService.isOnline
             .takeUntil(this.onDestroyEvent)
-            .subscribe(v => this.vortexIsOnline = v);
-
+            .subscribe(v => this.vortexIsOnline = v)
     }
-
+    
     ngOnInit() {
-
         this.tupleLoader = this.vortexService
             .createTupleLoader(this, this.userDataFilt)
-            .observable.subscribe(tuples => this.user = <UserTuple>tuples[0]);
-
+            .observable
+            .subscribe(tuples => this.user = <UserTuple>tuples[0])
     }
-
+    
     logoutClicked(): void {
-
         this.vortexService.sendPayload(
             new Payload(Object.assign({logout: true}, this.userDataFilt))
-        );
-        setTimeout(() => location.reload(), 100);
+        )
+        setTimeout(() => location.reload(), 100)
     }
-
-
 }
