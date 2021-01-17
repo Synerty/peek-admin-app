1d83f244490ee46fbb56c99a4c75e528039e7800
Jarrod Chesney <jarrod.chesney@synerty.com>
2021-01-17 23:35:39 +1000

    PEEK-909: Update base-js / vortexjs dependencies
    Hooked up VortexStatusService to BalloonMsgService

--- peek_admin_app/src/app/app.component.ts
+++ peek_admin_app/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from "@angular/core"
-import { VortexService } from "@synerty/vortexjs"
+import { VortexService, VortexStatusService , NgLifeCycleEvents} from "@synerty/vortexjs"
+import { BalloonMsgService } from "@synerty/peek-plugin-base-js"
+import { takeUntil } from "rxjs-compat/operator/takeUntil";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-    constructor(private vortexService: VortexService) {
+export class AppComponent implements OnInit extends NgLifeCycleEvents{
+    constructor(private vortexService: VortexService,
+                private vortexStatusService: VortexStatusService,
+                private balloonMsg: BalloonMsgService,
+    ) {
+        super();
+        vortexStatusService.errors
+            .pipe(takeUntil(this.onDestroyEvent))
+            .subscribe((msg:string) => balloonMsg.showError(msg))
+        
+        vortexStatusService.warning
+            .pipe(takeUntil(this.onDestroyEvent))
+            .subscribe((msg:string) => balloonMsg.showWarning(msg))
+        
     }
     
     ngOnInit() {
