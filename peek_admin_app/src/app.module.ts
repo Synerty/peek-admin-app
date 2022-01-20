eae65570224d5b05e00606e6bde63762401e6aef
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-01-20 20:23:26 +1000

    PEEK-1380 NEW: Manage offline cache in peek-core-device

--- peek_admin_app/src/app.module.ts
+++ peek_admin_app/src/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser"
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { NgModule } from "@angular/core"
-import { FormsModule } from "@angular/forms"
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import {
     BalloonMsgService,
-    BalloonMsgModule
-} from "@synerty/peek-plugin-base-js"
+    BalloonMsgModule,
+} from "@synerty/peek-plugin-base-js";
 import {
     TupleStorageFactoryService,
     TupleStorageFactoryServiceWeb,
@@ -13,24 +13,24 @@ import {
     VortexStatusService,
     WebSqlBrowserFactoryService,
     WebSqlFactoryService,
-} from "@synerty/vortexjs"
-import { AppRoutingModule } from "./app/app-routing.module"
-import { AppComponent } from "./app/app.component"
-import { DashboardModule } from "./app/dashboard/dashboard.module"
-import { NavbarModule } from "./app/navbar/navbar.module"
-import { PluginRootComponent } from "./app/plugin-root.component"
-import { en_US, NZ_I18N } from "ng-zorro-antd/i18n"
+} from "@synerty/vortexjs";
+import { AppRoutingModule } from "./app/app-routing.module";
+import { AppComponent } from "./app/app.component";
+import { DashboardModule } from "./app/dashboard/dashboard.module";
+import { NavbarModule } from "./app/navbar/navbar.module";
+import { PluginRootComponent } from "./app/plugin-root.component";
+import { en_US, NZ_I18N } from "ng-zorro-antd/i18n";
 
-import { ACE_CONFIG, AceConfigInterface, AceModule } from "ngx-ace-wrapper"
+import { ACE_CONFIG, AceConfigInterface, AceModule } from "ngx-ace-wrapper";
 /** config angular i18n **/
-import { registerLocaleData } from "@angular/common"
-import en from "@angular/common/locales/en"
-import { NzIconModule } from "ng-zorro-antd/icon"
-import { HttpClientModule } from "@angular/common/http"
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { HttpClientModule } from "@angular/common/http";
 
-const DEFAULT_ACE_CONFIG: AceConfigInterface = {}
+const DEFAULT_ACE_CONFIG: AceConfigInterface = {};
 
-registerLocaleData(en)
+registerLocaleData(en);
 
 @NgModule({
     declarations: [AppComponent, PluginRootComponent],
@@ -47,7 +47,7 @@ registerLocaleData(en)
         NavbarModule,
     ],
     providers: [
-        {provide: NZ_I18N, useValue: en_US},
+        { provide: NZ_I18N, useValue: en_US },
         {
             provide: ACE_CONFIG,
             useValue: DEFAULT_ACE_CONFIG,
@@ -67,4 +67,9 @@ registerLocaleData(en)
     bootstrap: [AppComponent],
 })
 export class AppModule {
+    constructor(private vortexStatusService: VortexStatusService) {
+        this.vortexStatusService.httpSessionLoggedOut.subscribe(() => {
+            setTimeout(() => location.reload(), 100);
+        });
+    }
 }
