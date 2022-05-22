ca60100bbe6c96111a5d3b7629f23b8ca4a22617
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-05-22 19:18:49 +1000

    PEEK-1534 Add worker support

--- peek_admin_app/src/main.ts
+++ peek_admin_app/src/main.ts
@@ -3,16 +3,17 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app.module";
 import { environment } from "./environments/environment";
-import { VortexService } from "@synerty/vortexjs";
-
-if (environment.production) {
-    enableProdMode();
-}
+import { Payload, PayloadDelegateWeb, VortexService } from "@synerty/vortexjs";
 
 const protocol = location.protocol.toLowerCase() == "https:" ? "wss" : "ws";
 VortexService.setVortexUrl(
     `${protocol}://${location.hostname}:${location.port}/vortexws`
 );
 VortexService.setVortexClientName("peek-admin-app");
+
+Payload.setWorkerDelegate(new PayloadDelegateWeb());
+if (environment.production) {
+    enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
